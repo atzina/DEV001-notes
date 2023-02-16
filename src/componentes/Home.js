2758fcefcb099e2f71950ad9a8a6301237203832
atzina <atzina@gmail.com>
2023-02-15 21:17:54 -0600

    las rutas y comiezo de css

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { loginWhithGoogle } from "../firebase";
+import App from "../App";
+import { useNavigate } from "react-router-dom";
+import styles from "../style/Home.module.css";
 
 function Home() {
-    return (
+  const navigate = useNavigate();
+
+  const handleGoogleSignin = async (credentials) => {
+    console.log(credentials);
+    await loginWhithGoogle();
+    navigate("/wall");
+  };
+
+  return (
     <div>
-        <h1>home</h1>
+      <body>
+        <div className={styles.divTitle}>
+          <h1 className={styles.title}>Diario de Viaje</h1>
+        </div>
+        <img
+          className={styles.carroviaje}
+          src="https://i.postimg.cc/76VTTZbp/home-Sin-Espacios.png"
+          alt="carroviaje"
+        />
+        <div className={styles.login}>
+          <h4>inicia sesión con</h4>
+          <button className={styles.btnGoggle} onClick={handleGoogleSignin}>
+            Google
+          </button>
+        </div>
+      </body>
     </div>
-    );
+  );
 }
 
 export default Home;
